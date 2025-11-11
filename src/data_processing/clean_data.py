8014ac2322587aaffd1f6d000cff3e56aa397c89
Eg0Mak <egoship@yandex.ru>
2025-11-11 16:45:17 +0300

    edit clean_data.py

--- src/data_processing/clean_data.py
+++ src/data_processing/clean_data.py
@@ -18,10 +18,18 @@ def clean_text(text: str, preserve_paragraphs: bool = False) -> str:
     # Убираем управляющие символы и невидимые пробелы
     text = text.replace("\xa0", " ")
     text = re.sub(r"[©®™℠]", " ", text)
+    text = text.replace("₽", "рублей")
+    text = text.replace("$", "долларов")
+    text = text.replace("→", "-")
 
-    # Убирает длинные последовательности точек и _
+    # Добавляем эквивалентное описание
+    text = text.replace('дм³', 'дм³ (кубических дециметров)')
+
+    # Убирает длинные последовательности точек, '_', '-'
     text = re.sub(r"\.{3,}", " ", text)
     text = re.sub(r"\_{2,}", " ", text)
+    text = re.sub(r"\-{3,}", " ", text)
+
 
     # Удаляем мусорные символы, но сохраняем нужные для e-mail, телефонов и знаков
     text = re.sub(r"[«»\"“”‘’\[\]{}<>|•■◆►]", " ", text)
@@ -34,14 +42,16 @@ def clean_text(text: str, preserve_paragraphs: bool = False) -> str:
         text = re.sub(r"\n{2,}", "\n\n", text)
         text = re.sub(r"[ \t]+", " ", text)
     
-    # Убираем пробелы перед знаками пунктуации и двойные пробелы
+    # Убираем пробелы перед знаками пунктуации
     text = re.sub(r"\s+([,.!?;:])", r"\1", text)
+
+    # Убираем двойные пробелы
     text = re.sub(r" {2,}", " ", text)
 
     return text.strip()
 
 def clean_csv(input_filename: str):
-    """CSV processing: text cleaning, removing gaps and duplicates"""
+    """Обработка CSV: очистка текстов, удаление пропусков и дублей"""
     input_path = os.path.join(RAW_DATA_DIR, input_filename)
     output_path = os.path.join(PROCESSED_DATA_DIR, "clean_data.csv")
 
