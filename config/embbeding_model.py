b00c6052ef186974361cae1e2fa3a43838416365
Eg0Mak <egoship@yandex.ru>
2025-11-11 21:19:57 +0300

    add embbeding_model

--- config/embbeding_model.py
+++ config/embbeding_model.py
@@ -1,14 +1,12 @@
 import os
 from dotenv import load_dotenv
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-
+from sentence_transformers import SentenceTransformer
 
 load_dotenv()
-
 
 EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME", "intfloat/multilingual-e5-base")
 
 
-embed_model = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL_NAME)
+embed_model = SentenceTransformer("intfloat/multilingual-e5-large")
 
 print(f"Загружена модель эмбеддингов: {EMBEDDING_MODEL_NAME}")
